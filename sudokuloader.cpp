a5badb81173279b4b120c6447b9dd081b5201882
Jerome Wirth <jeromewirth@gmx.de>
2016-03-06 23:45:33 +0100

    sudokuloader class handles loading of a new level

--- sudokuloader.cpp
+++ sudokuloader.cpp
@@ -1,7 +1,36 @@
 #include "sudokuloader.h"
 
-SudokuLoader::SudokuLoader()
+SudokuLoader::SudokuLoader(QString fileName)
 {
+    int row = 0;
 
+    QFile file(fileName);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        qDebug() << "Could not read file" << endl;
+
+    QTextStream in(&file);
+    while (!in.atEnd()) {
+        QString line = in.readLine();
+
+
+        for (int col = 0; col < 9; col++) {
+            m_board[row][col] = line.at(col).digitValue();
+        }
+
+        row++;
+    }
 }
 
+void SudokuLoader::printLevel()
+{
+    for (int row = 0; row < 9; row++) {
+        for (int col = 0; col < 9; col++) {
+            qDebug() << "Row: " << row << ", Col: " << col << " Num: " << m_board[row][col];
+        }
+    }
+}
+
+int SudokuLoader::getNumber(int row, int col)
+{
+    return m_board[row][col];
+}
--- sudokuloader.h
+++ sudokuloader.h
@@ -1,15 +1,20 @@
 #ifndef SUDOKULOADER_H
 #define SUDOKULOADER_H
 
+#include <QWidget>
+#include <QFile>
+#include <QDebug>
+#include <QTextStream>
 
 class SudokuLoader
 {
 public:
-    SudokuLoader();
+    SudokuLoader(QString fileName);
+    void printLevel();
+    int getNumber(int row, int col);
 
-signals:
-
-public slots:
+private:
+    int m_board[9][9];
 };
 
 #endif // SUDOKULOADER_H
